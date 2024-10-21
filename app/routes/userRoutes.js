ab9aaa894625425c7f75428c16b1a61a6e5474a3
Saurabh Srivastava <srivastava.sau@northeastern.edu>
2024-10-21 19:00:34 -0400

    Update userRoutes.js
    check

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -27,7 +27,7 @@ router.route('/healthz')
 // Create a new user
 router.post('/v2/user', userController.createUser);
 
-router.route('/v1/user/self')
+router.route('/v2/user/self')
 .get(userController.authenticateUser, userController.getUserInfo)
 .put(userController.authenticateUser, userController.updateUser)
   .head((req, res) => {
